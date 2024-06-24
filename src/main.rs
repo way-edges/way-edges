2fb194ea1846dcbd7e24dd58d42b60570efbe1b3
ogios <2134692955@qq.com>
2024-06-24 22:29:08 +0800

    fix: gtk command line process before clap result in gui fail to render

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,39 @@ mod data;
 mod ui;
 
 use clap::Parser;
-use gio::prelude::*;
-use std::{thread, time::Duration};
+use gio::{prelude::*, ApplicationFlags};
+use gtk::Application;
 
 fn main() {
+    // for cmd line help msg.
+    // or else it will show help from `gtk` not `clap`
+    args::Cli::parse();
+
+    // set renderer explicitly to cairo instead of ngl
     std::env::set_var("GSK_RENDERER", "cairo");
 
-    let application = gtk::Application::new(None::<String>, Default::default());
+    // that flag is for command line arguments
+    let application = gtk::Application::new(None::<String>, ApplicationFlags::HANDLES_OPEN);
 
+    // when args passed, `open` will be signaled instead of `activate`
+    application.connect_open(|app, _, _| {
+        init_app(app);
+    });
     application.connect_activate(|app| {
-        let args = args::Cli::parse();
-        let group_map = config::get_config().unwrap();
-        let cfgs = config::match_group_config(group_map, args.group);
-
-        activate::activate(app, cfgs);
+        init_app(app);
     });
 
     application.run();
+}
 
-    loop {
-        thread::sleep(Duration::from_millis(1000));
-    }
+fn init_app(app: &Application) {
+    let args = args::Cli::parse();
+    println!("{:#?}", args);
+    let group_map = config::get_config().unwrap();
+    let cfgs = config::match_group_config(group_map, args.group);
+    cfgs.iter().for_each(|c| {
+        println!("{}", c.debug());
+    });
+
+    activate::activate(app, cfgs);
 }
