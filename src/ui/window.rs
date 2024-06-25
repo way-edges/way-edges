a79f543d7c8915aeb62d0f04470f41465d802379
ogios <2134692955@qq.com>
2024-06-25 20:30:47 +0800

    implement margin in window; add config schema & example config;

--- src/ui/window.rs
+++ src/ui/window.rs
@@ -6,31 +6,35 @@ use gtk4_layer_shell::{Layer, LayerShell};
 
 pub fn new_window(app: &Application, mut config: Config) -> gtk::ApplicationWindow {
     let window = gtk::ApplicationWindow::new(app);
-    let size = config.size;
 
+    // init layer
     window.init_layer_shell();
-
     window.set_layer(Layer::Top);
 
-    // Push other windows out of the way
-    // window.auto_exclusive_zone_enable();
-
+    // edge and position
     window.set_anchor(config.edge, true);
     if let Some(pos) = config.position {
         window.set_anchor(pos, true);
     }
 
+    // margin
+    config.margins.clone().iter().for_each(|m| {
+        window.set_margin(m.0, m.1);
+    });
+
     draw_area::setup_draw(
         &window,
         config.edge,
-        size,
+        config.size,
         config.event_map.take().unwrap(),
         config.color,
         config.extra_trigger_size,
         config.transition_duration,
         config.frame_rate,
     );
+    drop(config);
 
+    // set something after show
     window.connect_show(move |w: &gtk::ApplicationWindow| {
         // transparency background !! may not work for some gtk4 theme, and idk what to do with it !!
         let provider = CssProvider::new();
