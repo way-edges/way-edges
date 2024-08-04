1132c28237e7cc38657b6a0b4f8d3583adbe80c7
ogios <ogios@foxmail.com>
2024-08-04 20:29:51 +0800

    make sure daemon can only be one and send notify if other tries to create another

--- src/daemon.rs
+++ src/daemon.rs
@@ -1,4 +1,11 @@
-use std::{cell::RefCell, collections::HashMap, io, path::Path, rc::Rc, thread};
+use std::{
+    cell::{Cell, RefCell},
+    collections::HashMap,
+    io,
+    path::Path,
+    rc::Rc,
+    thread,
+};
 
 use gio::{
     glib::{self, clone::Downgrade, WeakRef},
@@ -139,23 +146,45 @@ fn new_app() -> (GroupMapCtxRc, Application) {
 
     let group_map = Rc::new(RefCell::new(GroupMapCtx::new()));
 
+    let is_already_active = Rc::new(Cell::new(false));
+
     // when args passed, `open` will be signaled instead of `activate`
     app.connect_open(glib::clone!(
         #[weak]
         group_map,
+        #[strong]
+        is_already_active,
         move |app, _, _| {
-            debug!("connect open");
-            on_active(app);
-            group_map.borrow_mut().inited(app);
+            if is_already_active.get() {
+                notify_send(
+                    "Way-edges",
+                    "A way-edges daemon already running, something trys to run one more",
+                    true,
+                );
+            } else {
+                is_already_active.set(true);
+                debug!("connect open");
+                on_active(app);
+                group_map.borrow_mut().inited(app);
+            }
         }
     ));
     app.connect_activate(glib::clone!(
         #[weak]
         group_map,
         move |app| {
-            debug!("connect activate");
-            on_active(app);
-            group_map.borrow_mut().inited(app);
+            if is_already_active.get() {
+                notify_send(
+                    "Way-edges",
+                    "A way-edges daemon already running, something trys to run one more",
+                    true,
+                );
+            } else {
+                is_already_active.set(true);
+                debug!("connect activate");
+                on_active(app);
+                group_map.borrow_mut().inited(app);
+            }
         }
     ));
     (group_map, app)
