ef11de2628a3c771142902d4eef27ac28f27a8d7
ogios <ogios@foxmail.com>
2024-11-10 20:26:39 +0800

    add smithay client toolkit to watch for wayland outputs(monitors status)

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,33 @@ mod ipc_command;
 mod plug;
 mod ui;
 
+use std::sync::atomic::AtomicPtr;
+
 use file_watch::*;
 use ipc_command::send_command;
 use notify_rust::Notification;
 
-// NOTE: thread 0
+static MAIN_RUNTIME_HANDLE: AtomicPtr<tokio::runtime::Handle> =
+    AtomicPtr::new(std::ptr::null_mut());
 
+fn get_main_runtime_handle() -> &'static tokio::runtime::Handle {
+    return unsafe {
+        MAIN_RUNTIME_HANDLE
+            .load(std::sync::atomic::Ordering::Acquire)
+            .as_ref()
+            .unwrap()
+    };
+}
+
+// NOTE: thread 0
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
+    let main_runtime_handle = tokio::runtime::Handle::current();
+    MAIN_RUNTIME_HANDLE.store(
+        Box::into_raw(Box::new(main_runtime_handle)),
+        std::sync::atomic::Ordering::Release,
+    );
+
     // completion script output, and exit
     args::if_print_completion_and_exit();
 
