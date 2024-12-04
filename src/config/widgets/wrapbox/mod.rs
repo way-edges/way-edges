dad6e37752bac0bd99a21bdd5e35c185e5983259
ogios <ogios@foxmail.com>
2024-12-04 18:16:17 +0800

    add align

--- src/config/widgets/wrapbox/mod.rs
+++ src/config/widgets/wrapbox/mod.rs
@@ -67,6 +67,64 @@ pub enum Align {
     BottomRight,
 }
 
+pub type AlignFuncPos = (f64, f64);
+pub type AlignFuncGridBlockSize = (f64, f64);
+pub type AlignFuncContentSize = (f64, f64);
+pub type AlignFunc =
+    Box<fn(AlignFuncPos, AlignFuncGridBlockSize, AlignFuncContentSize) -> AlignFuncPos>;
+
+impl Align {
+    pub fn to_func(&self) -> AlignFunc {
+        macro_rules! align_y {
+            (T, $pos:expr, $size:expr, $content_size:expr) => {
+                $pos.1
+            };
+            (C, $pos:expr, $size:expr, $content_size:expr) => {
+                $pos.1 + ($size.1 - $content_size.1) / 2.
+            };
+            (B, $pos:expr, $size:expr, $content_size:expr) => {
+                $pos.1 + ($size.1 - $content_size.1)
+            };
+        }
+
+        macro_rules! align_x {
+            (L, $pos:expr, $size:expr, $content_size:expr) => {
+                $pos.0
+            };
+            (C, $pos:expr, $size:expr, $content_size:expr) => {
+                $pos.0 + ($size.0 - $content_size.0) / 2.
+            };
+            (R, $pos:expr, $size:expr, $content_size:expr) => {
+                $pos.0 + ($size.0 - $content_size.0)
+            };
+        }
+
+        macro_rules! a {
+            ($x:tt $y:tt) => {
+                |pos, size, content_size| {
+                    (
+                        align_x!($x, pos, size, content_size),
+                        align_y!($y, pos, size, content_size),
+                    )
+                }
+            };
+        }
+
+        Box::new(match self {
+            #[allow(unused)]
+            Align::TopLeft => a!(L T),
+            Align::TopCenter => a!(C T),
+            Align::TopRight => a!(R T),
+            Align::CenterLeft => a!(L C),
+            Align::CenterCenter => a!(C C),
+            Align::CenterRight => a!(R C),
+            Align::BottomLeft => a!(L B),
+            Align::BottomCenter => a!(C B),
+            Align::BottomRight => a!(R B),
+        })
+    }
+}
+
 #[derive(Deserialize, Debug)]
 pub struct BoxSelf {
     #[serde(default = "dt_gap")]
