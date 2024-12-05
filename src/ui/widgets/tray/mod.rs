3891daca080f226c30afe07a57e436df55e59379
ogios <ogios@foxmail.com>
2024-12-05 13:34:17 +0800

    menu defination

--- src/ui/widgets/tray/mod.rs
+++ src/ui/widgets/tray/mod.rs
@@ -1,10 +1,17 @@
-use std::{cell::RefCell, collections::HashMap, rc::Rc};
+use std::{
+    cell::RefCell,
+    collections::{HashMap, HashSet},
+    rc::Rc,
+};
 
 use cairo::ImageSurface;
 
 use crate::{
     config::widgets::wrapbox::Align,
-    plug::tray::{register_tray, unregister_tray, TrayIcon, TrayItem, TrayMenu},
+    plug::{
+        self,
+        tray::{register_tray, unregister_tray, TrayIcon, TrayItem, TrayMenu},
+    },
 };
 
 use super::wrapbox::{
@@ -12,7 +19,42 @@ use super::wrapbox::{
     expose::{BoxExpose, BoxRedrawFunc},
 };
 
-struct Menu {}
+struct MenuState {
+    open_state: HashSet<i32>,
+    hover_state: i32,
+}
+impl MenuState {
+    fn filter_state_with_new_menu(&mut self, menu: &RootMenu) {}
+}
+
+struct RootMenu {
+    id: i32,
+    submenus: Vec<Menu>,
+}
+
+struct CommonMenu {
+    pub id: i32,
+    pub label: Option<String>,
+    pub enabled: bool,
+    pub icon: Option<TrayIcon>,
+}
+
+enum Menu {
+    Radio {
+        common: CommonMenu,
+        choosed: bool,
+    },
+    Check {
+        common: CommonMenu,
+        checked: bool,
+    },
+    WithSubmenu {
+        common: CommonMenu,
+        submenu: Vec<Menu>,
+    },
+    Normal(CommonMenu),
+    Separator,
+}
 
 struct Tray {
     tray_id: TrayID,
@@ -21,13 +63,41 @@ struct Tray {
     icon: ImageSurface,
     menu_path: Option<String>,
 
-    menu: Option<Menu>,
+    menu: Option<(RootMenu, MenuState)>,
+
+    updated: bool,
+    content: ImageSurface,
 }
 
 impl Tray {
-    fn update_title(&mut self, title: Option<String>) {}
-    fn update_icon(&mut self, icon: TrayIcon) {}
-    fn update_menu(&mut self, menu: Menu) {}
+    fn update_title(&mut self, title: Option<String>) {
+        if title != self.title {
+            self.title = title;
+            self.set_updated();
+        }
+    }
+    fn update_icon(&mut self, icon: ImageSurface) {
+        self.icon = icon;
+        self.set_updated();
+    }
+    fn update_menu(&mut self, new: RootMenu) {
+        if let Some((old, state)) = &mut self.menu {
+            state.filter_state_with_new_menu(&new);
+            *old = new;
+        }
+    }
+}
+impl Tray {
+    fn set_updated(&mut self) {
+        self.updated = true;
+    }
+    fn redraw_if_updated(&mut self) {
+        if self.updated {
+            self.draw();
+            self.updated = false;
+        }
+    }
+    fn draw(&mut self) {}
 }
 impl DisplayWidget for Tray {
     fn get_size(&self) -> (f64, f64) {
@@ -99,6 +169,8 @@ struct TrayModule {
     grid: GridBox<TrayID>,
     id_tray_map: HashMap<TrayID, Tray>,
     redraw_signal: BoxRedrawFunc,
+
+    icon_size: i32,
 }
 impl TrayModule {
     fn draw_content(&mut self) -> ImageSurface {
@@ -113,11 +185,13 @@ impl TrayModule {
             grid,
             id_tray_map: HashMap::new(),
             redraw_signal,
+
+            icon_size: 16,
         }
     }
     fn add_tray(&mut self, id: String, tray_item: &TrayItem) {
         let id = Rc::new(id);
-        let tray = Self::parse_tary_item(id.clone(), tray_item, 16);
+        let tray = Self::parse_tary_item(id.clone(), tray_item, self.icon_size);
 
         self.grid.add(id.clone());
         self.id_tray_map.insert(id, tray);
@@ -131,15 +205,19 @@ impl TrayModule {
         (self.redraw_signal)()
     }
 
+    fn find_tray(&mut self, id: &String) -> Option<&mut Tray> {
+        self.id_tray_map.get_mut(id)
+    }
+
     fn parse_tray_icon(value: &TrayIcon, size: i32) -> ImageSurface {
         value
             .get_icon_with_size(size)
             .unwrap_or(ImageSurface::create(cairo::Format::ARgb32, size, size).unwrap())
     }
-    fn parse_tary_item(tray_id: TrayID, value: &TrayItem, size: i32) -> Tray {
+    fn parse_tary_item(tray_id: TrayID, value: &TrayItem, icon_size: i32) -> Tray {
         let id = value.id.clone();
         let title = value.title.clone();
-        let icon = Self::parse_tray_icon(&value.icon, size);
+        let icon = Self::parse_tray_icon(&value.icon, icon_size);
         let menu_path = value.menu_path.clone();
         Tray {
             tray_id,
@@ -149,6 +227,16 @@ impl TrayModule {
             menu_path,
             menu: None,
         }
+    }
+    fn parse_menu(menu: &plug::tray::Menu, icon_size: i32) -> Menu {}
+    fn parse_tray_menu(tray_menu: &TrayMenu, icon_size: i32) -> RootMenu {
+        let id = tray_menu.id as i32;
+        let submenus = tray_menu
+            .menus
+            .iter()
+            .map(|menu| TrayModule::parse_menu(menu, icon_size))
+            .collect();
+        RootMenu { id, submenus }
     }
 }
 
@@ -216,9 +304,19 @@ pub fn init_tray(expose: &BoxExpose) -> Rc<RefCell<TrayCtx>> {
                 Event::ItemRemove => {
                     ctx.module.remove_tray(id);
                 }
-                _ => {} // Event::TitleUpdate(title) => todo!(),
-                        // Event::IconUpdate(tray_icon) => todo!(),
-                        // Event::MenuNew(tray_menu) => todo!(),
+                Event::TitleUpdate(title) => {
+                    if let Some(tray) = ctx.module.find_tray(id) {
+                        tray.update_title(title.clone());
+                    }
+                }
+                Event::IconUpdate(tray_icon) => {
+                    let size = ctx.module.icon_size;
+                    if let Some(tray) = ctx.module.find_tray(id) {
+                        tray.update_icon(TrayModule::parse_tray_icon(tray_icon, size));
+                    }
+                }
+                Event::MenuNew(tray_menu) => {}
+                _ => {} // Event::MenuNew(tray_menu) => todo!(),
             }
         }
     )));
