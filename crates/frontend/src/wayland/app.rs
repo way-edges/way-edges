41ac48946cb3529f3497c4df02d4fad90fcd4330
ogios <ogios@foxmail.com>
2025-01-16 22:14:55 +0800

    widget builder

--- crates/frontend/src/wayland/app.rs
+++ crates/frontend/src/wayland/app.rs
@@ -1,20 +1,36 @@
 use std::{
+    cell::{Cell, UnsafeCell},
     collections::HashMap,
-    ops::DerefMut,
-    sync::{Arc, Mutex, MutexGuard},
+    rc::Rc,
+    sync::{mpsc::Receiver, Arc, Mutex, MutexGuard},
+    time::Duration,
 };
 
 use backend::ipc::IPCCommand;
-use calloop::{LoopHandle, LoopSignal};
+use calloop::{
+    channel::Sender,
+    ping::{make_ping, Ping},
+    LoopHandle, LoopSignal,
+};
+use config::MonitorSpecifier;
+use glib::clone::{Downgrade, Upgrade};
 use smithay_client_toolkit::{
-    compositor::CompositorState,
+    compositor::{CompositorState, SurfaceData as SctkSurfaceData, SurfaceDataExt},
     output::OutputState,
     registry::RegistryState,
     seat::SeatState,
-    shell::wlr_layer::{LayerShell, LayerSurface},
+    shell::{
+        wlr_layer::{LayerShell, LayerSurface},
+        WaylandSurface,
+    },
     shm::{slot::SlotPool, Shm},
 };
-use wayland_client::{protocol::wl_pointer, QueueHandle};
+use wayland_client::{
+    protocol::{wl_pointer, wl_surface::WlSurface},
+    Proxy, QueueHandle,
+};
+
+use crate::animation::{AnimationList, ToggleAnimationRc};
 
 pub struct App {
     pub exit: bool,
@@ -88,13 +104,29 @@ impl App {
         .ok()
     }
 }
+impl App {
+    fn signal_redraw(&self, layer: &LayerSurface) {
+        layer
+            .wl_surface()
+            .frame(&self.queue_handle, layer.wl_surface().clone());
+    }
+}
 
 pub struct Group {
     pub widgets: HashMap<String, Arc<Mutex<Widget>>>,
 }
 impl Group {
     fn init_group(widgets_config: Vec<config::Config>, app: &App) -> Result<Self, String> {
-        todo!()
+        let widgets = widgets_config
+            .into_iter()
+            .map(|cfg| {
+                let widget_name = cfg.name.clone();
+                let window_ctx = Widget::init_widget(cfg, app)?;
+                Ok((widget_name, window_ctx))
+            })
+            .collect::<Result<HashMap<String, Arc<Mutex<Widget>>>, String>>()?;
+
+        Ok(Self { widgets })
     }
     fn get_widget(&self, name: &str) -> Option<MutexGuard<Widget>> {
         self.widgets.get(name).map(|w| w.lock().unwrap())
@@ -109,6 +141,127 @@ impl Widget {
     fn toggle_pin(&mut self) {
         todo!()
     }
+    fn init_widget(conf: config::Config, app: &App) -> Result<Arc<Mutex<Self>>, String> {
+        let surface = app.compositor_state.create_surface_with_data(
+            &app.queue_handle,
+            SurfaceData {
+                sctk: SctkSurfaceData::new(None, 1),
+                widget: weak.clone(),
+            },
+        );
+
+        // Arc::new_cyclic(|weak| {
+        //     SurfaceData::from_wl()
+        //     Mutex::new(s)
+        // })
+
+        todo!()
+    }
+}
+
+pub struct WidgetBuilder<'a> {
+    name: String,
+    monitor: MonitorSpecifier,
+    app: &'a App,
+    layer: LayerSurface,
+
+    has_update: Rc<Cell<bool>>,
+
+    pop_animation: ToggleAnimationRc,
+    animation_list: AnimationList,
+    pop_state: Rc<UnsafeCell<Option<Rc<()>>>>,
+}
+impl<'a> WidgetBuilder<'a> {
+    pub fn new_animation(&mut self, time_cost: u64) -> ToggleAnimationRc {
+        self.animation_list.new_transition(time_cost)
+    }
+    pub fn extend_animation_list(&mut self, list: &AnimationList) {
+        self.animation_list.extend_list(list);
+    }
+    pub fn make_pop_func(&mut self, pop_duration: u64) -> Ping {
+        let (ping, source) = make_ping().unwrap();
+
+        let layer = self.layer.clone();
+        let pop_animation = self.pop_animation.downgrade();
+        let pop_state = Rc::downgrade(&self.pop_state);
+        let pop_duration = Duration::from_millis(pop_duration);
+
+        self.app
+            .event_loop_handle
+            .insert_source(source, move |_, _, app| {
+                // pop up
+                let guard_weak = {
+                    let Some(pop_animation) = pop_animation.upgrade() else {
+                        return;
+                    };
+                    let Some(pop_state) = pop_state.upgrade() else {
+                        return;
+                    };
+
+                    let guard = Rc::new(());
+                    let guard_weak = Rc::downgrade(&guard);
+                    unsafe { pop_state.get().as_mut().unwrap().replace(guard) };
+
+                    pop_animation
+                        .borrow_mut()
+                        .set_direction(crate::animation::ToggleDirection::Forward);
+                    app.signal_redraw(&layer);
+
+                    guard_weak
+                };
+
+                // hide
+                let layer = layer.clone();
+                let pop_animation = pop_animation.clone();
+                app.event_loop_handle.insert_source(
+                    calloop::timer::Timer::from_duration(pop_duration),
+                    move |_, _, app| {
+                        let Some(pop_animation) = pop_animation.upgrade() else {
+                            return calloop::timer::TimeoutAction::Drop;
+                        };
+                        if guard_weak.upgrade().is_none() {
+                            return calloop::timer::TimeoutAction::Drop;
+                        }
+
+                        pop_animation
+                            .borrow_mut()
+                            .set_direction(crate::animation::ToggleDirection::Backward);
+                        app.signal_redraw(&layer);
+
+                        calloop::timer::TimeoutAction::Drop
+                    },
+                );
+            });
+
+        ping
+    }
+    pub fn make_redraw_notifier(&self, mut func: Option<impl FnMut(&mut App) + 'static>) -> Ping {
+        let (ping, source) = make_ping().unwrap();
+
+        let has_update = Rc::downgrade(&self.has_update);
+        let layer = self.layer.clone();
+
+        self.app
+            .event_loop_handle
+            .insert_source(source, move |_, _, app| {
+                let Some(has_update) = has_update.upgrade() else {
+                    return;
+                };
+                has_update.set(true);
+
+                if let Some(func) = func.as_mut() {
+                    func(app)
+                };
+
+                // signal redraw
+                app.signal_redraw(&layer);
+            });
+
+        ping
+    }
+}
+impl<'a> WidgetBuilder<'a> {
+    fn new(conf: &config::Config, app: &'a App) -> Result<WidgetBuilder<'a>, String> {}
 }
 
 // TODO: we are not really access this in multithreaded situation, so we don't need
@@ -116,3 +269,18 @@ impl Widget {
 // We can test for using Rc&RefCell, but it's not really a significant overhead when comparing to
 // refresh rate(even 240hz still needs 4.9ms, but the overhead from lock is only nanoseconds)
 // pub struct WidgetPtr(Rc<RefCell<Widget>>);
+
+pub struct SurfaceData {
+    pub sctk: SctkSurfaceData,
+    pub widget: std::sync::Weak<Mutex<Widget>>,
+}
+impl SurfaceDataExt for SurfaceData {
+    fn surface_data(&self) -> &SctkSurfaceData {
+        &self.sctk
+    }
+}
+impl SurfaceData {
+    pub fn from_wl(wl: &WlSurface) -> &Self {
+        wl.data::<SurfaceData>().unwrap()
+    }
+}
