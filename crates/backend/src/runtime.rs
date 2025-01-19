34cab868c317b89054ff8dc265a2b63b8610a889
ogios <ogios@foxmail.com>
2025-01-19 16:41:45 +0800

    use local runtime

--- crates/backend/src/runtime.rs
+++ crates/backend/src/runtime.rs
@@ -1,9 +1,17 @@
-use std::sync::atomic::{AtomicBool, AtomicPtr};
+use std::sync::atomic::AtomicPtr;
 
-use tokio::runtime::Handle;
+use tokio::runtime::{Handle, LocalRuntime};
 
-static TASK_HANDLER_INITED: AtomicBool = AtomicBool::new(false);
+static LOCAL_RUNTIME: AtomicPtr<LocalRuntime> = AtomicPtr::new(std::ptr::null_mut());
 static TASK_HANDLER: AtomicPtr<Handle> = AtomicPtr::new(std::ptr::null_mut());
+pub fn get_backend_runtime() -> &'static LocalRuntime {
+    unsafe {
+        LOCAL_RUNTIME
+            .load(std::sync::atomic::Ordering::Relaxed)
+            .as_ref()
+            .unwrap()
+    }
+}
 pub fn get_backend_runtime_handle() -> &'static Handle {
     unsafe {
         TASK_HANDLER
@@ -17,19 +25,23 @@ pub fn init_backend_runtime_handle() {
     let (created, is_created) = tokio::sync::oneshot::channel();
 
     std::thread::spawn(|| {
-        let rt = tokio::runtime::Builder::new_current_thread()
-            .enable_all()
-            .build()
-            .unwrap();
+        // let rt = tokio::runtime::Builder::new_current_thread()
+        //     .enable_all()
+        //     .build()
+        //     .unwrap();
+        let rt = tokio::runtime::LocalRuntime::new().unwrap();
 
         TASK_HANDLER.store(
             Box::into_raw(Box::new(rt.handle().clone())),
             std::sync::atomic::Ordering::Relaxed,
         );
-        TASK_HANDLER_INITED.store(true, std::sync::atomic::Ordering::Relaxed);
+        LOCAL_RUNTIME.store(
+            Box::into_raw(Box::new(rt)),
+            std::sync::atomic::Ordering::Relaxed,
+        );
 
         created.send(()).unwrap();
-        rt.block_on(std::future::pending::<()>());
+        get_backend_runtime().block_on(std::future::pending::<()>());
     });
 
     is_created.blocking_recv().unwrap();
