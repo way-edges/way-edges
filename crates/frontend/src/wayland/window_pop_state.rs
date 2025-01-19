9601b9ebed2e7c4c02b058c4c18c082aa7afeca7
ogios <ogios@foxmail.com>
2025-01-19 18:14:25 +0800

    mouse event

---
+++ crates/frontend/src/wayland/window_pop_state.rs
@@ -0,0 +1,53 @@
+use std::{cell::UnsafeCell, rc::Rc, time::Duration};
+
+use crate::animation::{ToggleAnimationRc, ToggleDirection};
+
+#[derive(Debug)]
+pub struct WindowPopState {
+    pub pin_state: bool,
+    pub pop_state: Rc<UnsafeCell<Option<Rc<()>>>>,
+    pub pop_animation: ToggleAnimationRc,
+    pub pin_key: u32,
+    pub pop_duration: Duration,
+}
+impl WindowPopState {
+    pub fn new(ani: ToggleAnimationRc, pop_state: Rc<UnsafeCell<Option<Rc<()>>>>) -> Self {
+        Self {
+            pin_state: false,
+            pop_state,
+            pop_animation: ani,
+            pin_key: 0x112,
+            pop_duration: Duration::from_secs(1),
+        }
+    }
+    pub fn invalidate_pop(&mut self) {
+        unsafe { drop(self.pop_state.get().as_mut().unwrap().take()) };
+    }
+    pub fn toggle_pin(&mut self, is_hovering: bool) {
+        self.invalidate_pop();
+        let state = !self.pin_state;
+        self.pin_state = state;
+        if is_hovering {
+            return;
+        }
+        self.pop_animation.borrow_mut().set_direction(state.into());
+    }
+    pub fn enter(&mut self) {
+        self.invalidate_pop();
+        if self.pin_state {
+            return;
+        }
+        self.pop_animation
+            .borrow_mut()
+            .set_direction(ToggleDirection::Forward);
+    }
+    pub fn leave(&mut self) {
+        self.invalidate_pop();
+        if self.pin_state {
+            return;
+        }
+        self.pop_animation
+            .borrow_mut()
+            .set_direction(ToggleDirection::Backward);
+    }
+}
