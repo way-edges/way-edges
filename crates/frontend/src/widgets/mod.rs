f5189b84fc63d29aa7ed4275d60adc5e78e85ccb
ogios <ogios@foxmail.com>
2025-01-20 11:51:52 +0800

    clippy

--- crates/frontend/src/widgets/mod.rs
+++ crates/frontend/src/widgets/mod.rs
@@ -41,9 +41,9 @@ fn process_config(conf: &mut config::Config, size: (i32, i32)) {
     }
 }
 
-pub fn init_widget<'a>(
+pub fn init_widget(
     conf: &mut config::Config,
-    builder: &mut WidgetBuilder<'a>,
+    builder: &mut WidgetBuilder,
 ) -> Box<dyn WidgetContext> {
     let monitor = builder.app.output_state.info(&builder.output).unwrap();
     let size = monitor.modes[0].dimensions;
@@ -53,25 +53,25 @@ pub fn init_widget<'a>(
     match conf.widget.take().unwrap() {
         config::widgets::Widget::Btn(btn_config) => {
             log::debug!("initializing button");
-            let w = button::init_widget(builder, size, &conf, btn_config);
+            let w = button::init_widget(builder, size, conf, btn_config);
             log::info!("initialized button");
             Box::new(w)
         }
         config::widgets::Widget::Slider(slide_config) => {
             log::debug!("initializing slider");
-            let w = slide::init_widget(builder, size, &conf, slide_config);
+            let w = slide::init_widget(builder, size, conf, slide_config);
             log::info!("initialized slider");
             w
         }
         config::widgets::Widget::HyprWorkspace(hypr_workspace_config) => {
             log::debug!("initializing hypr-workspace");
-            let w = hypr_workspace::init_widget(builder, size, &conf, hypr_workspace_config);
+            let w = hypr_workspace::init_widget(builder, size, conf, hypr_workspace_config);
             log::info!("initialized hypr-workspace");
             Box::new(w)
         }
         config::widgets::Widget::WrapBox(box_config) => {
             log::debug!("initializing box");
-            let w = wrapbox::init_widget(builder, &conf, box_config);
+            let w = wrapbox::init_widget(builder, conf, box_config);
             log::info!("initialized box");
             Box::new(w)
         }
