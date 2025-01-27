e3baaaa806e1b5f804d00f9409ef1f5b84f10bc2
ogios <ogios@foxmail.com>
2025-01-27 10:37:41 +0800

    combine workspace

---
+++ crates/backend/src/workspace/mod.rs
@@ -0,0 +1,55 @@
+use std::collections::HashMap;
+
+use calloop::channel::Sender;
+
+pub mod hypr_workspace;
+pub mod niri_workspace;
+
+#[derive(Debug, Clone, Copy)]
+pub struct WorkspaceData {
+    /// workspace len, start from 1
+    pub workspace_count: i32,
+    /// index, start from 0
+    pub focus: i32,
+}
+impl Default for WorkspaceData {
+    fn default() -> Self {
+        WorkspaceData {
+            workspace_count: 1,
+            focus: 0,
+        }
+    }
+}
+
+type ID = u32;
+
+struct WorkspaceCtx {
+    id_cache: ID,
+    cb: HashMap<ID, Sender<WorkspaceData>>,
+    current: WorkspaceData,
+}
+
+impl WorkspaceCtx {
+    fn new() -> Self {
+        Self {
+            cb: HashMap::new(),
+            id_cache: 0,
+            current: WorkspaceData::default(),
+        }
+    }
+    fn add_cb(&mut self, cb: Sender<WorkspaceData>) -> ID {
+        let id = self.id_cache;
+        cb.send(self.current).unwrap();
+        self.cb.insert(id, cb);
+        self.id_cache += 1;
+        id
+    }
+    fn remove_cb(&mut self, id: ID) {
+        self.cb.remove(&id);
+    }
+    fn call(&mut self) {
+        self.cb.values_mut().for_each(|f| {
+            f.send(self.current).unwrap();
+        })
+    }
+}
