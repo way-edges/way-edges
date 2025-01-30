0c44e55be1bcbc444c727c5e11d2ef7b0b36d5af
ogios <ogios@foxmail.com>
2025-01-30 18:51:36 +0800

    tray

--- crates/backend/src/tray/icon.rs
+++ crates/backend/src/tray/icon.rs
@@ -1,26 +1,11 @@
 use std::{io::Cursor, path::PathBuf, sync::LazyLock};
 
 use cairo::ImageSurface;
-use gdk::{
-    gdk_pixbuf::{Colorspace, Pixbuf},
-    prelude::GdkCairoContextExt,
-};
+use resvg::{tiny_skia, usvg};
 use system_tray::item::IconPixmap;
 
-use util::Z;
+use util::{pre_multiply_and_to_little_endian_argb, Z};
 
-fn new_image_surface_from_buf(buf: Pixbuf) -> ImageSurface {
-    let width = buf.width();
-    let height = buf.height();
-    let format = cairo::Format::ARgb32;
-    let surf = ImageSurface::create(format, width, height).unwrap();
-    let context = cairo::Context::new(&surf).unwrap();
-
-    context.set_source_pixbuf(&buf, Z, Z);
-    context.paint().unwrap();
-
-    surf
-}
 fn scale_image_to_size(img: ImageSurface, size: i32) -> ImageSurface {
     let scale = size as f64 / img.height() as f64;
     let width = (img.width() as f64 * scale).ceil() as i32;
@@ -35,17 +20,76 @@ fn scale_image_to_size(img: ImageSurface, size: i32) -> ImageSurface {
 
     surf
 }
+
 fn draw_icon_file(file_path: PathBuf, size: i32) -> Option<ImageSurface> {
-    let pixbuf = Pixbuf::from_file(file_path)
-        .inspect_err(|e| {
-            log::error!("draw_icon_file error: {e}");
-        })
-        .ok()?;
-    Some(scale_image_to_size(
-        new_image_surface_from_buf(pixbuf),
-        size,
-    ))
+    let ext = file_path.extension().unwrap().to_str().unwrap();
+    let img = match ext {
+        "png" => load_png(&file_path),
+        "svg" => load_svg(&file_path),
+        _ => {
+            log::error!("draw_icon_file error: unsupported file extension: {ext}");
+            return None;
+        }
+    }?;
+
+    Some(scale_image_to_size(img, size))
 }
+
+fn load_png(p: &PathBuf) -> Option<ImageSurface> {
+    let contents = std::fs::read(p)
+        .inspect_err(|e| log::error!("load_png error: {e}"))
+        .ok()?;
+    ImageSurface::create_from_png(&mut Cursor::new(contents))
+        .inspect_err(|f| log::error!("load_png to surface error: {f}"))
+        .ok()
+}
+
+fn load_svg(p: &PathBuf) -> Option<ImageSurface> {
+    let tree = {
+        let opt = usvg::Options {
+            resources_dir: std::fs::canonicalize(p)
+                .ok()
+                .and_then(|p| p.parent().map(|p| p.to_path_buf())),
+            ..usvg::Options::default()
+        };
+        // NOTE: WE DO NOT EXPECT TEXT TO APPEAR INSIDE, SHOULD WE?
+        // opt.fontdb.load_system_fonts();
+
+        let svg_data = std::fs::read(p)
+            .inspect_err(|f| log::error!("load_svg error: {f}"))
+            .ok()?;
+        usvg::Tree::from_data(&svg_data, &opt)
+            .inspect_err(|f| log::error!("parse svg data error: {f}"))
+            .ok()?
+    };
+
+    let pixmap_size = tree.size().to_int_size();
+    let mut pixmap = tiny_skia::Pixmap::new(pixmap_size.width(), pixmap_size.height()).unwrap();
+    resvg::render(&tree, tiny_skia::Transform::default(), &mut pixmap.as_mut());
+
+    // RGBA
+    let mut pixels = pixmap.take();
+
+    // TO ARGB
+    for i in (0..pixels.len()).step_by(4) {
+        let alpha = pixels[i];
+        pixels[i] = pixels[i + 1];
+        pixels[i + 1] = pixels[i + 2];
+        pixels[i + 2] = pixels[i + 3];
+        pixels[i + 3] = alpha;
+    }
+
+    ImageSurface::create_for_data(
+        pixels,
+        cairo::Format::ARgb32,
+        pixmap_size.width() as i32,
+        pixmap_size.height() as i32,
+        pixmap_size.width() as i32 * 4,
+    )
+    .inspect_err(|f| log::error!("load_svg to surface error: {f}"))
+    .ok()
+}
+
 static DEFAULT_ICON_THEME: LazyLock<Option<String>> = LazyLock::new(linicon_theme::get_icon_theme);
 
 pub fn fallback_icon(size: i32, theme: Option<&str>) -> Option<ImageSurface> {
@@ -77,36 +121,48 @@ pub fn parse_icon_given_data(vec: &Vec<u8>) -> Option<ImageSurface> {
 pub fn parse_icon_given_pixmaps(vec: &[IconPixmap], size: i32) -> Option<ImageSurface> {
     if vec.is_empty() {
         None
-        // parse_icon_given_name("image-missing", size)
     } else {
         // we can do endian convert, but it's too hard
         // https://stackoverflow.com/a/10588779/21873016
 
         let pixmap = vec.last().unwrap();
+
+        // ARGB
         let mut pixels = pixmap.pixels.clone();
 
-        // from ironbar
+        // pre multiply
         for i in (0..pixels.len()).step_by(4) {
-            let alpha = pixels[i];
-            pixels[i] = pixels[i + 1];
-            pixels[i + 1] = pixels[i + 2];
-            pixels[i + 2] = pixels[i + 3];
-            pixels[i + 3] = alpha;
+            let res = pre_multiply_and_to_little_endian_argb([
+                pixels[i + 1],
+                pixels[i + 2],
+                pixels[i + 3],
+                pixels[i],
+            ]);
+
+            // little endian (BGRA)
+            // pixels[i] = res[3];
+            // pixels[i + 1] = res[0];
+            // pixels[i + 2] = res[1];
+            // pixels[i + 3] = res[2];
+            pixels[i] = res[0];
+            pixels[i + 1] = res[1];
+            pixels[i + 2] = res[2];
+            pixels[i + 3] = res[3];
+            // pixels[i] = res[0];
+            // pixels[i + 1] = res[1];
+            // pixels[i + 2] = res[2];
+            // pixels[i + 3] = res[3];
         }
 
-        let pixbuf = Pixbuf::from_mut_slice(
-            &mut pixels,
-            Colorspace::Rgb,
-            true,
-            8,
+        let img = ImageSurface::create_for_data(
+            pixels,
+            cairo::Format::ARgb32,
             pixmap.width,
             pixmap.height,
             pixmap.width * 4,
-        );
+        )
+        .unwrap();
 
-        Some(scale_image_to_size(
-            new_image_surface_from_buf(pixbuf),
-            size,
-        ))
+        Some(scale_image_to_size(img, size))
     }
 }
