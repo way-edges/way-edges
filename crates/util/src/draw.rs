1bfa3cf5614be0145c882570de3c700214e1de45
ogios <ogios@foxmail.com>
2025-01-30 16:21:04 +0800

    color transition

--- crates/util/src/draw.rs
+++ crates/util/src/draw.rs
@@ -1,28 +1,11 @@
 use std::f64::consts::PI;
 
 use cairo::{Format, ImageSurface, Path};
-use gdk::RGBA;
 
 use crate::Z;
 
 pub fn new_surface(size: (i32, i32)) -> ImageSurface {
     ImageSurface::create(Format::ARgb32, size.0, size.1).unwrap()
-}
-
-pub fn color_transition(start_color: RGBA, stop_color: RGBA, v: f32) -> RGBA {
-    let r = start_color.red() + (stop_color.red() - start_color.red()) * v;
-    let g = start_color.green() + (stop_color.green() - start_color.green()) * v;
-    let b = start_color.blue() + (stop_color.blue() - start_color.blue()) * v;
-    let a = start_color.alpha() + (stop_color.alpha() - start_color.alpha()) * v;
-    RGBA::new(r, g, b, a)
-}
-
-pub fn color_mix(one: RGBA, two: RGBA) -> RGBA {
-    let a = 1. - (1. - one.alpha()) * (1. - two.alpha());
-    let r = (one.red() * one.alpha() + two.red() * two.alpha() * (1. - one.alpha())) / a;
-    let g = (one.green() * one.alpha() + two.green() * two.alpha() * (1. - one.alpha())) / a;
-    let b = (one.blue() * one.alpha() + two.blue() * two.alpha() * (1. - one.alpha())) / a;
-    RGBA::new(r, g, b, a)
 }
 
 pub fn draw_rect_path(radius: f64, size: (f64, f64), corners: [bool; 4]) -> Result<Path, String> {
