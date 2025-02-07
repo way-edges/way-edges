e46e336456a4db2811660002280281796f345fdb
ogios <ogios@foxmail.com>
2025-02-07 17:59:55 +0800

    item in backend

--- crates/backend/src/tray/mod.rs
+++ crates/backend/src/tray/mod.rs
@@ -1,6 +1,7 @@
 mod context;
 mod event;
 pub mod icon;
+mod item;
 
 pub use context::{init_tray_client, register_tray, unregister_tray};
 pub use event::{tray_about_to_show_menuitem, tray_active_request, Event};
