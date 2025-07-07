12f81fc334555b65ac54e34d6ecc2c00b2f061cb
ogios <ogios@foxmail.com>
2025-07-07 17:19:32 +0800

    watch config file directly which supports symlink

--- crates/backend/src/config_file_watch.rs
+++ crates/backend/src/config_file_watch.rs
@@ -3,49 +3,57 @@ use std::{sync::Arc, time::Duration};
 use calloop::channel::Sender;
 use config::get_config_path;
 use futures_util::StreamExt;
-use inotify::{Inotify, WatchMask};
+use inotify::{EventMask, Inotify, WatchMask};
+use log::info;
 
 use crate::runtime::get_backend_runtime_handle;
 
 pub fn start_configuration_file_watcher(sender: Sender<()>) {
     get_backend_runtime_handle().spawn(async move {
-        let inotify = Inotify::init().unwrap();
-        let path = get_config_path().parent().unwrap();
-        let config_name = get_config_path().file_name().unwrap();
-
-        inotify
-            .watches()
-            .add(
-                path,
-                WatchMask::CREATE | WatchMask::MODIFY | WatchMask::DELETE,
-            )
-            .unwrap();
-
         let mut debouncer = None;
 
-        let mut buffer = [0; 1024];
-        let mut stream = inotify.into_event_stream(&mut buffer).unwrap();
-        while let Some(event_or_error) = stream.next().await {
-            let event = event_or_error.unwrap();
-            let Some(name) = event.name else {
-                continue;
-            };
-            if name.as_os_str() != config_name {
-                continue;
-            }
+        loop {
+            let inotify = Inotify::init().unwrap();
+            let file_path = get_config_path();
 
-            let new_d = Arc::new(());
-            let weak_d = Arc::downgrade(&new_d);
-            debouncer.replace(new_d);
+            inotify
+                .watches()
+                .add(
+                    file_path,
+                    WatchMask::CREATE
+                        | WatchMask::MODIFY
+                        | WatchMask::DELETE
+                        | WatchMask::DELETE_SELF
+                        | WatchMask::MOVE
+                        | WatchMask::MOVE_SELF
+                        | WatchMask::ATTRIB,
+                )
+                .unwrap();
 
-            let sender = sender.clone();
-            tokio::spawn(async move {
-                tokio::time::sleep(Duration::from_millis(700)).await;
-                if weak_d.upgrade().is_none() {
-                    return;
+            let mut buffer = [0; 1024];
+            let mut stream = inotify.into_event_stream(&mut buffer).unwrap();
+            while let Some(event_or_error) = stream.next().await {
+                let event = event_or_error.unwrap();
+                info!("Received inotify event: {event:?}");
+
+                let new_d = Arc::new(());
+                let weak_d = Arc::downgrade(&new_d);
+                debouncer.replace(new_d);
+
+                let sender = sender.clone();
+                tokio::spawn(async move {
+                    tokio::time::sleep(Duration::from_millis(700)).await;
+                    if weak_d.upgrade().is_none() {
+                        return;
+                    }
+                    println!("aaa");
+                    sender.send(()).unwrap();
+                });
+
+                if event.mask == EventMask::IGNORED {
+                    break;
                 }
-                sender.send(()).unwrap();
-            });
+            }
         }
     });
 }
