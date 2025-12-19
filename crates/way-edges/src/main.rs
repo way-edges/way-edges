02c9de8c8c319e18bf8d795c03af5447d9b76759
ogios <ogios@foxmail.com>
2025-12-19 19:11:15 +0800

    less log spam

--- crates/way-edges/src/main.rs
+++ crates/way-edges/src/main.rs
@@ -1,16 +1,43 @@
 mod args;
 
 use frontend::run_app;
+use log::Level;
 use std::env;
+use std::io::Write;
 
 fn main() {
     // completion script output, and exit
     args::if_print_completion_and_exit();
 
     if env::var("RUST_LOG").is_err() {
-        env::set_var("RUST_LOG", "info")
+        unsafe { env::set_var("RUST_LOG", "info,system_tray=error,zbus=warn") }
     }
-    env_logger::init();
+
+    // force tracing warn
+    unsafe {
+        env::set_var(
+            "RUST_LOG",
+            format!("{},tracing=warn,usvg=error", env::var("RUST_LOG").unwrap()),
+        )
+    };
+
+    eprintln!("Logging with settings: {}", env::var("RUST_LOG").unwrap());
+
+    env_logger::Builder::from_default_env()
+        .format(|buf, record| {
+            let (tag, color) = match record.level() {
+                Level::Debug => ("DBG", "\x1b[90m"), // grey
+                Level::Info => ("INF", "\x1b[34m"),  // blue
+                Level::Warn => ("WRN", "\x1b[33m"),  // yellow
+                Level::Error => ("ERR", "\x1b[31m"), // red
+                Level::Trace => ("TRC", "\x1b[2m"),
+            };
+
+            writeln!(buf, "{}{}:\x1b[0m {}", color, tag, record.args())
+        })
+        .init();
+
+    // env_logger::init();
 
     let cli = args::get_args();
 
