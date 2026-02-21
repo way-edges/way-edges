261265aa318c921911f0faae309a853de7411c4d
ogios <ogios@foxmail.com>
2026-02-21 15:07:30 +0800

    aaa

--- crates/config/src/kdl/mod.rs
+++ crates/config/src/kdl/mod.rs
@@ -3,11 +3,12 @@ use knus::Decode;
 pub mod common;
 mod shared;
 mod util;
-mod widgets;
+pub mod widgets;
 
 #[derive(Debug, Clone, Decode)]
 pub enum TopLevelConf {
     Btn(Btn),
+    Workspace(Workspace),
 }
 
 #[derive(Debug, Clone)]
@@ -15,7 +16,6 @@ pub struct Btn {
     pub common: common::CommonConfig,
     pub widget: widgets::button::BtnConfig,
 }
-
 impl<S: knus::traits::ErrorSpan> knus::Decode<S> for Btn {
     fn decode_node(
         node: &knus::ast::SpannedNode<S>,
@@ -24,6 +24,23 @@ impl<S: knus::traits::ErrorSpan> knus::Decode<S> for Btn {
         Ok(Self {
             common: common::CommonConfig::decode_node(node, ctx)?,
             widget: widgets::button::BtnConfig::decode_node(node, ctx)?,
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Workspace {
+    pub common: common::CommonConfig,
+    pub widget: widgets::workspace::WorkspaceConfig,
+}
+impl<S: knus::traits::ErrorSpan> knus::Decode<S> for Workspace {
+    fn decode_node(
+        node: &knus::ast::SpannedNode<S>,
+        ctx: &mut knus::decode::Context<S>,
+    ) -> Result<Self, knus::errors::DecodeError<S>> {
+        Ok(Self {
+            common: common::CommonConfig::decode_node(node, ctx)?,
+            widget: widgets::workspace::WorkspaceConfig::decode_node(node, ctx)?,
         })
     }
 }
