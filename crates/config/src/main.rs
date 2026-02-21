e65fb32fa02e2b0386e7a58443a47e21c72469b8
ogios <ogios@foxmail.com>
2026-02-21 14:17:33 +0800

    aaa

--- crates/config/src/main.rs
+++ crates/config/src/main.rs
@@ -1,19 +1,39 @@
-use knus::Decode;
+use std::env;
+
+use log::Level;
+
+use crate::kdl::TopLevelConf;
+use std::io::Write;
 
 mod kdl;
 
-#[derive(Debug, Clone, Decode)]
-pub enum TopLevelConf {
-    Btn(Btn),
-}
-
-#[derive(Debug, Clone, Decode)]
-pub struct Btn {
-    #[knus(flatten(child))]
-    pub common: kdl::common::CommonConfig,
-}
-
 fn main() {
+    if env::var("RUST_LOG").is_err() {
+        unsafe { env::set_var("RUST_LOG", "info,system_tray=error,zbus=warn") }
+    }
+
+    // force tracing warn
+    unsafe {
+        env::set_var(
+            "RUST_LOG",
+            format!("{},tracing=warn,usvg=error", env::var("RUST_LOG").unwrap()),
+        )
+    };
+
+    env_logger::Builder::from_default_env()
+        .format(|buf, record| {
+            let (tag, color) = match record.level() {
+                Level::Debug => ("DBG", "\x1b[90m"), // grey
+                Level::Info => ("INF", "\x1b[34m"),  // blue
+                Level::Warn => ("WRN", "\x1b[33m"),  // yellow
+                Level::Error => ("ERR", "\x1b[31m"), // red
+                Level::Trace => ("TRC", "\x1b[2m"),
+            };
+
+            writeln!(buf, "{}{}:\x1b[0m {}", color, tag, record.args())
+        })
+        .init();
+
     let config = match knus::parse::<Vec<TopLevelConf>>("aaa", include_str!("../debug.kdl")) {
         Ok(config) => config,
         Err(e) => {
@@ -22,6 +42,4 @@ fn main() {
         }
     };
     println!("{:#?}", config);
-
-    println!("Hello, world!");
 }
