12488b45df64be9ef3cc63c079c564a15e1b5830
ogios <ogios@foxmail.com>
2026-02-21 15:07:32 +0800

    aaa

--- crates/config/src/kdl/widgets/mod.rs
+++ crates/config/src/kdl/widgets/mod.rs
@@ -1,9 +1,9 @@
-use button::BtnConfig;
+// use button::BtnConfig;
 // use slide::base::SlideConfig;
 // use workspace::WorkspaceConfig;
 // use wrapbox::BoxConfig;
 
 pub mod button;
 // pub mod slide;
-// pub mod workspace;
+pub mod workspace;
 // pub mod wrapbox;
