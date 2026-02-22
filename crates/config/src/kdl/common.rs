d130d06fc9b0d62f5e2d6e90f1e0c4260cf75746
ogios <ogios@foxmail.com>
2026-02-22 18:08:27 +0800

    cb common

--- crates/config/src/kdl/common.rs
+++ crates/config/src/kdl/common.rs
@@ -1,8 +1,7 @@
-use knus::{
-    errors::DecodeError,
-    traits::{DecodePartial, ErrorSpan},
-    Decode,
-};
+use knus::{errors::DecodeError, Decode};
+use schemars::{json_schema, JsonSchema};
+use serde::{Deserialize, Deserializer};
+use serde_jsonrc::Value;
 use smithay_client_toolkit::shell::wlr_layer::{Anchor, Layer};
 use std::collections::HashSet;
 use std::ops::Deref;
@@ -13,7 +12,7 @@ use crate::kdl::shared::{Curve, NumOrRelative};
 pub enum MonitorSpecifier {
     Lists {
         ids: HashSet<usize>,
-        namses: HashSet<String>,
+        names: HashSet<String>,
     },
     All,
 }
@@ -21,7 +20,7 @@ impl Default for MonitorSpecifier {
     fn default() -> Self {
         Self::Lists {
             ids: HashSet::from([0]),
-            namses: HashSet::new(),
+            names: HashSet::new(),
         }
     }
 }
@@ -82,72 +81,206 @@ impl<S: knus::traits::ErrorSpan> knus::Decode<S> for MonitorSpecifier {
             }
         }
 
-        Ok(MonitorSpecifier::Lists { ids, namses: names })
+        Ok(MonitorSpecifier::Lists { ids, names })
+    }
+}
+impl JsonSchema for MonitorSpecifier {
+    fn schema_name() -> std::borrow::Cow<'static, str> {
+        "MonitorSpecifier".into()
+    }
+
+    fn json_schema(_: &mut schemars::SchemaGenerator) -> schemars::Schema {
+        json_schema!({
+            "oneOf": [
+                {
+                    "type": "string",
+                },
+                {
+                    "enum": ["*"],
+                },
+                {
+                    "type": "number",
+                    "minimum": 0,
+                },
+                {
+                    "type": "array",
+                    "items": {
+                        "type": ["string", "number"],
+                    },
+                }
+            ],
+        })
+    }
+}
+impl<'de> Deserialize<'de> for MonitorSpecifier {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        struct MonitorSpecifierVisitor;
+        impl<'ae> serde::de::Visitor<'ae> for MonitorSpecifierVisitor {
+            type Value = MonitorSpecifier;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str("a monitor ID or a list of monitor names")
+            }
+
+            fn visit_u64<E>(self, value: u64) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                Ok(MonitorSpecifier::Lists {
+                    ids: HashSet::from([value as usize]),
+                    names: HashSet::new(),
+                })
+            }
+
+            fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                if value == "*" {
+                    Ok(MonitorSpecifier::All)
+                } else {
+                    Ok(MonitorSpecifier::Lists {
+                        ids: HashSet::new(),
+                        names: HashSet::from([value.to_string()]),
+                    })
+                }
+            }
+
+            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+            where
+                A: serde::de::SeqAccess<'ae>,
+            {
+                let mut ids = HashSet::new();
+                let mut names = HashSet::new();
+                while let Some(value) = seq.next_element::<Value>()? {
+                    match value {
+                        Value::String(s) => {
+                            if s == "*" {
+                                return Err(serde::de::Error::invalid_value(
+                                    serde::de::Unexpected::Str(&s),
+                                    &"You cannot use the wildcard character '*' in a list of monitors, it is only allowed as the sole argument to specify all monitors",
+                                ));
+                            }
+                            names.insert(s);
+                        }
+                        Value::Number(num) => {
+                            if let Some(id) = num.as_u64() {
+                                ids.insert(id as usize);
+                            } else {
+                                return Err(serde::de::Error::invalid_value(
+                                    serde::de::Unexpected::Other(&format!("number {}", num)),
+                                    &"Invalid integer value encountered",
+                                ));
+                            }
+                        }
+                        _ => {
+                            return Err(serde::de::Error::invalid_type(
+                                serde::de::Unexpected::Other(&format!("{:?}", value)),
+                                &"a string or a number",
+                            ));
+                        }
+                    }
+                }
+                Ok(MonitorSpecifier::Lists { ids, names })
+            }
+        }
+
+        deserializer.deserialize_any(MonitorSpecifierVisitor)
     }
 }
 
-#[derive(Debug, Clone, Default, Decode)]
+#[derive(Debug, Clone, Default, Decode, Deserialize, JsonSchema)]
 pub struct Margins {
     #[knus(child, default, unwrap(argument))]
+    #[serde(default)]
     pub left: NumOrRelative,
     #[knus(child, default, unwrap(argument))]
+    #[serde(default)]
     pub top: NumOrRelative,
     #[knus(child, default, unwrap(argument))]
+    #[serde(default)]
     pub right: NumOrRelative,
     #[knus(child, default, unwrap(argument))]
+    #[serde(default)]
     pub bottom: NumOrRelative,
 }
 
-#[derive(Debug, Clone, Decode)]
+#[derive(Debug, Clone, Decode, Deserialize, JsonSchema)]
+#[serde(rename_all = "kebab-case")]
+#[schemars(deny_unknown_fields)]
 pub struct CommonConfig {
     #[knus(child, unwrap(argument, decode_with = match_edge))]
+    #[serde(deserialize_with = "deserialize_edge")]
+    #[schemars(schema_with = "schema_edge")]
     pub edge: Anchor,
 
     #[knus(child, unwrap(argument, decode_with = match_edge))]
+    #[serde(default)]
+    #[serde(deserialize_with = "deserialize_optional_edge")]
+    #[schemars(schema_with = "schema_optional_edge")]
     pub position: Option<Anchor>,
 
-    #[knus(child, default=Layer::Top, unwrap(argument, decode_with = match_layer))]
+    #[knus(child, default=dt_layer(), unwrap(argument, decode_with = match_layer))]
+    #[serde(default = "dt_layer")]
+    #[serde(deserialize_with = "deserialize_layer")]
+    #[schemars(schema_with = "schema_layer")]
     pub layer: Layer,
 
     #[knus(child, default, unwrap(argument))]
+    #[serde(default)]
     pub offset: NumOrRelative,
 
     #[knus(child, default)]
+    #[serde(default)]
     pub margins: Margins,
 
     #[knus(child, default)]
+    #[serde(default)]
     pub monitor: MonitorSpecifier,
 
     #[knus(child, default, unwrap(argument))]
+    #[serde(default)]
     pub namespace: String,
 
     #[knus(child)]
+    #[serde(default)]
     pub ignore_exclusive: bool,
 
-    #[knus(child, default = 300, unwrap(argument))]
+    #[knus(child, default = dt_transition_duration(), unwrap(argument))]
+    #[serde(default = "dt_transition_duration")]
     pub transition_duration: u64,
 
     #[knus(child, default, unwrap(argument))]
+    #[serde(default)]
     pub animation_curve: Curve,
 
-    #[knus(child, default = NumOrRelative::Num(1.0), unwrap(argument))]
+    #[knus(child, default = dt_extra_trigger_size(), unwrap(argument))]
+    #[serde(default = "dt_extra_trigger_size")]
     pub extra_trigger_size: NumOrRelative,
 
-    #[knus(child, default = NumOrRelative::Num(0.0), unwrap(argument))]
+    #[knus(child, default = dt_preview_size(), unwrap(argument))]
+    #[serde(default = "dt_preview_size")]
     pub preview_size: NumOrRelative,
 
     // TODO: true
     #[knus(child)]
+    #[serde(default)]
     pub pinnable: bool,
 
     // TODO: true
     #[knus(child)]
+    #[serde(default)]
     pub pin_with_key: bool,
 
-    #[knus(child, default = smithay_client_toolkit::seat::pointer::BTN_MIDDLE, unwrap(argument))]
+    #[knus(child, default = dt_pin_key(), unwrap(argument))]
+    #[serde(default = "dt_pin_key")]
     pub pin_key: u32,
 
     #[knus(child)]
+    #[serde(default)]
     pub pin_on_startup: bool,
 }
 
@@ -181,6 +314,25 @@ impl CommonConfig {
     }
 }
 
+fn dt_edge() -> Anchor {
+    Anchor::LEFT
+}
+fn dt_layer() -> Layer {
+    Layer::Top
+}
+fn dt_transition_duration() -> u64 {
+    300
+}
+fn dt_extra_trigger_size() -> NumOrRelative {
+    NumOrRelative::Num(1.0)
+}
+fn dt_preview_size() -> NumOrRelative {
+    NumOrRelative::Num(0.0)
+}
+fn dt_pin_key() -> u32 {
+    smithay_client_toolkit::seat::pointer::BTN_MIDDLE
+}
+
 fn match_edge(edge: &str) -> Result<Anchor, std::io::Error> {
     Ok(match edge {
         "top" => Anchor::TOP,
@@ -208,5 +360,98 @@ fn match_layer(layer: &str) -> Result<Layer, std::io::Error> {
                 format!("Invalid layer: {}", layer),
             ))
         }
+    })
+}
+
+pub fn deserialize_optional_edge<'de, D>(d: D) -> Result<Option<Anchor>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    struct EventMapVisitor;
+    impl serde::de::Visitor<'_> for EventMapVisitor {
+        type Value = Option<Anchor>;
+
+        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+            formatter.write_str("edge only support: left, right, top, bottom")
+        }
+
+        fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+        where
+            E: serde::de::Error,
+        {
+            Ok(Some(match_edge(v).map_err(|_| {
+                serde::de::Error::invalid_value(serde::de::Unexpected::Str(v), &self)
+            })?))
+        }
+
+        fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+        where
+            E: serde::de::Error,
+        {
+            self.visit_str(v.as_str())
+        }
+    }
+
+    d.deserialize_any(EventMapVisitor)
+}
+
+pub fn deserialize_edge<'de, D>(d: D) -> Result<Anchor, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    if let Some(edge) = deserialize_optional_edge(d)? {
+        Ok(edge)
+    } else {
+        Err(serde::de::Error::missing_field("edge is not optional"))
+    }
+}
+
+pub fn deserialize_layer<'de, D>(d: D) -> Result<Layer, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    struct EventMapVisitor;
+    impl serde::de::Visitor<'_> for EventMapVisitor {
+        type Value = Layer;
+
+        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+            formatter.write_str("layer only support: background, bottom, top, overlay")
+        }
+
+        fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+        where
+            E: serde::de::Error,
+        {
+            match_layer(v)
+                .map_err(|_| serde::de::Error::invalid_value(serde::de::Unexpected::Str(v), &self))
+        }
+
+        fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
+        where
+            E: serde::de::Error,
+        {
+            self.visit_str(v.as_str())
+        }
+    }
+
+    d.deserialize_any(EventMapVisitor)
+}
+
+pub fn schema_edge(_: &mut schemars::SchemaGenerator) -> schemars::Schema {
+    json_schema!({
+        "type": "string",
+        "enum": ["top", "bottom", "left", "right"]
+    })
+}
+pub fn schema_optional_edge(_: &mut schemars::SchemaGenerator) -> schemars::Schema {
+    json_schema!({
+        "type": ["string", "null"],
+        "enum": ["top", "bottom", "left", "right"]
+    })
+}
+pub fn schema_layer(_: &mut schemars::SchemaGenerator) -> schemars::Schema {
+    json_schema!({
+        "type": "string",
+        "enum": ["top", "bottom", "background", "overlay"]
     })
 }
